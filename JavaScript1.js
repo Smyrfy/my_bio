e31f2cff90788f94adfcb884c8612d61a105eba4
Smyrfy <dudko.dasha72773@gmail.com>
2025-06-02 22:27:00 +0300

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('prankButton');
+    const image = document.getElementById('prankImage');
+    const audio = document.getElementById('prankAudio');
+
+    button.addEventListener('click', () => {
+        image.style.display = 'block';
+        audio.play();
+    });
+});
